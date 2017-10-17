a36504427f8bba6c04f3d64639a7ce9399c46aeb
PINET <PJ043331>
2017-10-17 17:33:08 +0200

    end of day

--- connexion.php
+++ connexion.php
@@ -1,32 +1,25 @@
 <!doctype html>
-<!--
-  Material Design Lite
-  Copyright 2015 Google Inc. All rights reserved.
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      https://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License
--->
 <html lang="fr">
   <?php
     include './inc/head_de_chaque_page.php';
   ?>
   <body>
-    <?php include './inc/header-template.php'; ?>
+    <?php include './inc/header_accueil.php'; ?>
     <main class="mdl-layout__content mdl-color--grey-100">
         <div class="mdl-grid demo-content">
-          <!-- mettre le code de la page ici -->
-        </div>
+          <h2>Veuillez fournir vos informations de connexion</h2>
+          <form action="#">
+            <div class="mdl-textfield mdl-js-textfield">
+              <input class="mdl-textfield__input" type="text" id="sample1">
+              <label class="mdl-textfield__label" for="sample1">Text...</label>
+            </div>
+          </form>
+        </div>    
       </main>
+      
     </div> <!-- ne pas suprrimer-->
+
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" style="position: fixed; left: -1000px; height: -1000px;">
         <defs>
           <mask id="piemask" maskContentUnits="objectBoundingBox">
